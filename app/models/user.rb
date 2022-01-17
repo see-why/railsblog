31745914bf11ae0146cd488d85579f17a79e2271
see-why <iyadicy@gmail.com>
2022-01-17 23:12:50 +0100

    Method that returns 3 most recent posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, through: :posts
   has_many :likes, through: :posts
+
+  def most_recent_posts
+    User.posts.last(3)
+  end
 end
