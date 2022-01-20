682e64a27c3c792593bd44ca1776fe0185b946d3
see-why <iyadicy@gmail.com>
2022-01-20 01:43:47 +0100

    Add data collections to action methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
-  def show; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
+  
+  def show
+    user = User.find(params[:user_id])
+    @post = user.posts.find(params[:id])
+  end
 end
