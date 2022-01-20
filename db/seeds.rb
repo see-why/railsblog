df123217f195d0fc718f744fd977838d78c02a24
see-why <iyadicy@gmail.com>
2022-01-20 01:44:09 +0100

    Add counter to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Post.destroy_all
-User.destroy_all
 Comment.destroy_all
 Like.destroy_all
+Post.destroy_all
+User.destroy_all
 
-Bruce = User.create!(name: 'Bruce Wayne', photo: Faker::LoremFlickr.image(size: "50x60") , bio: Faker::Lorem.paragraph, posts_counter: 0)
+
+Bruce = User.create!(name: 'Bruce Wayne', photo: Faker::LoremFlickr.image(size: "50x60") , bio: Faker::Lorem.paragraph, posts_counter: 3)
 Dick = User.create!(name: 'Dick Grayson', photo: Faker::LoremFlickr.image(size: "50x60"), bio: Faker::Lorem.paragraph, posts_counter: 0)
 Jason = User.create!(name: 'Jason Todd', photo: Faker::LoremFlickr.image(size: "50x60") , bio: Faker::Lorem.paragraph, posts_counter: 0)
 Tim = User.create!(name: 'Tim Drake', photo: Faker::LoremFlickr.image(size: "50x60") , bio: Faker::Lorem.paragraph, posts_counter: 0)
 
-post1 = Bruce.posts.create!(title: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 0).chop, text: Faker::Lorem.paragraph, comments_counter: 0, likes_counter: 0)
-post2 = Bruce.posts.create!(title: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 0).chop, text: Faker::Lorem.paragraph, comments_counter: 0, likes_counter: 0)
-post3 = Bruce.posts.create!(title: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 0).chop, text: Faker::Lorem.paragraph, comments_counter: 0, likes_counter: 0)
+post1 = Bruce.posts.create!(title: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 0).chop, text: Faker::Lorem.paragraph, comments_counter: 3, likes_counter: 1)
+post2 = Bruce.posts.create!(title: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 0).chop, text: Faker::Lorem.paragraph, comments_counter: 2, likes_counter: 1)
+post3 = Bruce.posts.create!(title: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 0).chop, text: Faker::Lorem.paragraph, comments_counter: 5, likes_counter: 1)
 
 Dick.comments.create!(text: 'really nice post Michael', post: post1)
 Jason.comments.create!(text: 'Well Done 👏👏👏', post: post1)
