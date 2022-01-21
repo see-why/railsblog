55c894a5be7b96b5ec2bb9c70f80bc24e6d68cb9
see-why <iyadicy@gmail.com>
2022-01-21 16:32:25 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'comments/create'
-  get 'comment/create'
+  get 'likes/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root 'users#index'
