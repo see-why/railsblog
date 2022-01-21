067a4349c7d78081a706b7fbeb38261e7eed585f
see-why <iyadicy@gmail.com>
2022-01-21 11:33:40 +0100

    Add create posts and comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'comment/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root 'users#index'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show create] do
+      resources :comments, only: %i[create]
+    end
   end
 end
