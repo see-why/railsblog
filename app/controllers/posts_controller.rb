b05fa071d2f09a6ba176742fc2d2030602f8088e
see-why <iyadicy@gmail.com>
2022-01-21 20:19:27 +0100

    Fixing n+1 challenges

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
     @user = User.find(params[:user_id])
-    @post = @user.posts.find(params[:id])
+    @post = @user.posts.includes(:comments).find(params[:id])
     @post_counter = params[:post_counter]
     @comment = Comment.new
   end
@@ -17,6 +17,8 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.new(params.require(:post).permit(:title, :text).merge(author_id: current_user.id))
+    @post.likes_counter = 0
+    @post.comments_counter = 0
     if @post.save
       flash[:alert] = 'Created New Post succesfully'
       redirect_to [:user_posts]
