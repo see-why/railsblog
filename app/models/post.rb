55a2921268a0090c7c36cd6c19143d8c9bd46b37
see-why <iyadicy@gmail.com>
2022-01-21 18:55:12 +0100

    Validations for model attributes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greated_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greated_than_or_equal_to: 0 }
+
   has_many :likes
   has_many :comments
   belongs_to :author, class_name: 'User'
