13a82fef3fee91425a80b52ad8441e15eb9425aa
see-why <iyadicy@gmail.com>
2022-01-21 16:22:46 +0100

    Update response message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
     current_user = post.author
     @comment = current_user.comments.new(params.require(:comment).permit(:text).merge(post_id: post.id))
     if @comment.save
-      flash[:success] = 'Created New Post succesfully'
+      flash[:success] = 'Created New Comment succesfully'
     else
       flash.now[:fail] = 'Failed to Create a New Comment'
     end
