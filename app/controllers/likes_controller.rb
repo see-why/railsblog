4dcf5e958d600637051d7ac68df859f92dc9dada
see-why <iyadicy@gmail.com>
2022-01-21 16:23:04 +0100

    Like controller create method

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,13 @@
+class LikesController < ApplicationController
+  def create
+    post = Post.find(params[:post_id])
+    current_user = post.author
+    @like = current_user.likes.new(post_id: post.id)
+    if @like.save
+      flash[:success] = 'Added new like'
+    else
+      flash.now[:fail] = 'Failed to add new like'
+    end
+    redirect_to [:user_post, { id: post.id.to_s, user_id: current_user.id.to_s}]
+  end
+end
