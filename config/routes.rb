4cccfeccdc7027ae43afb7d0dd9c6ce0f947d27a
see-why <iyadicy@gmail.com>
2022-01-21 12:29:07 +0100

    Comments and likes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'comments/create'
   get 'comment/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root 'users#index'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show create] do
+    resources :posts do
       resources :comments, only: %i[create]
+      resources :likes, only: %i[create]
     end
   end
 end
