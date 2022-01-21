726f0b0d02fc502d695eb43b25cfb1c3f5090940
see-why <iyadicy@gmail.com>
2022-01-21 12:26:33 +0100

    New and Create actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,20 @@ class PostsController < ApplicationController
     @post = @user.posts.find(params[:id])
     @post_counter = params[:post_counter]
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    current_user = User.find(params[:user_id])
+    @post = current_user.posts.new(params.require(:post).permit(:title, :text).merge(user_id: current_user.id))
+    if @post.save
+      @post.update_user_posts_counter
+      flash[:success] = 'Created New Post succesfully'
+      redirect_to [:user_posts]
+    else
+      redirect_to [:new_user_post]
+    end
+  end
 end
